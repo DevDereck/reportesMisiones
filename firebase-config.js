691544643d832b2dd185f56986d55449dfba0c50
Dereck Vargas Sibaja <devdereck08@gmail.com>
2026-02-12 17:23:58 -0600

    git commit -m "chore(main): fix firebase-config.js"

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAcWR8HHMsKOuwg6MMBNyo3I9QXWNYSVSE",
+    authDomain: "misione.firebaseapp.com",
+    projectId: "misione",
+    storageBucket: "misione.firebasestorage.app",
+    messagingSenderId: "1007040409964",
+    appId: "1:1007040409964:web:a50525738a714e80690349"
+  };
+
+export const adminAuthConfig = {
+  username: "adminicpacrMisiones",
+  password: "Efesios220Misiones",
+  email: "adminicpacrMisiones@icpacrmisiones.com"
+};
